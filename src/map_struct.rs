c665c278e52dbcc099b40958e2863492ba01a343
Skye Jensen <skyeajensen@gmail.com>
2021-05-31 17:23:03 -0400

    Unify get_attr functions functions and add a get_optional_child function that returns an `Option` instead of a `Result`

--- src/map_struct.rs
+++ src/map_struct.rs
@@ -1,9 +1,8 @@
+use celeste::binel::*;
+use std::borrow::Borrow;
+use std::collections::HashMap;
 use std::error::Error;
 use std::fmt;
-use celeste::binel::*;
-use std::collections::HashMap;
-use std::borrow::Borrow;
-
 
 #[derive(Debug)]
 pub struct Rect {
@@ -144,16 +143,6 @@ impl CelesteMapLevel {
     }
 }
 
-macro_rules! ok_when {
-    ($e:expr, $kind:expr) => {
-        if $e.is_err() && $e.unwrap_err().kind != $kind {
-            return Err($e.unwrap_err())
-        } else {
-            $e.ok()
-        }
-    }
-}
-
 
 macro_rules! expect_elem {
     ($elem:expr, $name:expr) => {
@@ -192,42 +181,43 @@ pub fn from_binfile(binfile: BinFile) -> Result<CelesteMap, CelesteMapError> {
 fn parse_level(elem: &BinEl) -> Result<CelesteMapLevel, CelesteMapError> {
     expect_elem!(elem, "level");
 
-    let x = require_present(get_attr_int(elem, "x"), &elem.name, "x")?;
-    let y = require_present(get_attr_int(elem, "y"), &elem.name, "y")?;
-    let width = require_present(get_attr_int(elem, "width"), &elem.name, "width")? as u32;
-    let height = require_present(get_attr_int(elem, "height"), &elem.name, "height")? as u32;
-    let object_tiles = ok_when!(get_child(elem, "fgtiles"), CelesteMapErrorType::MissingChild);
-    let fg_decals = ok_when!(get_child(elem, "fgdecals"), CelesteMapErrorType::MissingChild);
-    let bg_decals = ok_when!(get_child(elem, "bgdecals"), CelesteMapErrorType::MissingChild);
+    let x = get_attr(elem, "x")?;
+    let y = get_attr(elem, "y")?;
+    let width = get_attr::<i32>(elem, "width")? as u32;
+    let height = get_attr::<i32>(elem, "height")? as u32;
+    let object_tiles = get_optional_child(elem, "fgtiles");
+    let fg_decals = get_optional_child(elem, "fgdecals");
+    let bg_decals = get_optional_child(elem, "bgdecals");
 
     Ok(CelesteMapLevel {
         bounds: Rect {
             x, y, width, height,
         },
-        name: get_attr_text(elem, "name").and_then(|o|o.ok_or_else(||CelesteMapError::missing_attribute(&elem.name, "name")))?,
-        color: get_attr_int(elem, "c")?.unwrap_or_default(),
-        camera_offset_x: get_attr_float(elem, "cameraOffsetX")?.unwrap_or_default(),
-        camera_offset_y: get_attr_float(elem, "cameraOffsetY")?.unwrap_or_default(),
-        wind_pattern: get_attr_text(elem, "windPattern")?.unwrap_or_default(),
-        space: get_attr_bool(elem, "space")?.unwrap_or_default(),
-        underwater: get_attr_bool(elem, "underwater")?.unwrap_or_default(),
-        whisper: get_attr_bool(elem, "whisper")?.unwrap_or_default(),
-        dark: get_attr_bool(elem, "dark")?.unwrap_or_default(),
-        disable_down_transition: get_attr_bool(elem, "disableDownTransition")?.unwrap_or_default(),
+        name: get_attr(elem, "name")?,
+        color: get_optional_attr(elem, "c")?.unwrap_or_default(),
+        camera_offset_x: get_optional_attr(elem, "cameraOffsetX")?.unwrap_or_default(),
+        camera_offset_y: get_optional_attr(elem, "cameraOffsetY")?.unwrap_or_default(),
+        wind_pattern: get_optional_attr(elem, "windPattern")?.unwrap_or_default(),
+        space: get_optional_attr(elem, "space")?.unwrap_or_default(),
+        underwater: get_optional_attr(elem, "underwater")?.unwrap_or_default(),
+        whisper: get_optional_attr(elem, "whisper")?.unwrap_or_default(),
+        dark: get_optional_attr(elem, "dark")?.unwrap_or_default(),
+        disable_down_transition: get_optional_attr(elem, "disableDownTransition")?
+            .unwrap_or_default(),
 
-        music: get_attr_text(elem, "music")?.unwrap_or_default(),
-        alt_music: get_attr_text(elem, "alt_music")?.unwrap_or_default(),
-        ambience: get_attr_text(elem, "ambience")?.unwrap_or_default(),
+        music: get_optional_attr(elem, "music")?.unwrap_or_default(),
+        alt_music: get_optional_attr(elem, "alt_music")?.unwrap_or_default(),
+        ambience: get_optional_attr(elem, "ambience")?.unwrap_or_default(),
         music_layers: [
-            get_attr_bool(elem, "musicLayer1")?.unwrap_or_default(),
-            get_attr_bool(elem, "musicLayer2")?.unwrap_or_default(),
-            get_attr_bool(elem, "musicLayer3")?.unwrap_or_default(),
-            get_attr_bool(elem, "musicLayer4")?.unwrap_or_default(),
-            get_attr_bool(elem, "musicLayer5")?.unwrap_or_default(),
-            get_attr_bool(elem, "musicLayer6")?.unwrap_or_default(),
+            get_optional_attr(elem, "musicLayer1")?.unwrap_or_default(),
+            get_optional_attr(elem, "musicLayer2")?.unwrap_or_default(),
+            get_optional_attr(elem, "musicLayer3")?.unwrap_or_default(),
+            get_optional_attr(elem, "musicLayer4")?.unwrap_or_default(),
+            get_optional_attr(elem, "musicLayer5")?.unwrap_or_default(),
+            get_optional_attr(elem, "musicLayer6")?.unwrap_or_default(),
         ],
-        music_progress: get_attr_text(elem, "musicProgress")?.unwrap_or_default(),
-        ambience_progress: get_attr_text(elem, "ambienceProgress")?.unwrap_or_default(),
+        music_progress: get_optional_attr(elem, "musicProgress")?.unwrap_or_default(),
+        ambience_progress: get_optional_attr(elem, "ambienceProgress")?.unwrap_or_default(),
 
         fg_tiles: parse_fgbg_tiles(get_child(elem, "solids")?, width/8, height/8)?,
         bg_tiles: parse_fgbg_tiles(get_child(elem, "bg")?, width/8, height/8)?,
@@ -249,8 +239,8 @@ fn parse_level(elem: &BinEl) -> Result<CelesteMapLevel, CelesteMapError> {
 }
 
 fn parse_fgbg_tiles(elem: &BinEl, width: u32, height: u32) -> Result<Vec<char>, CelesteMapError> {
-    let offset_x = get_attr_int(elem, "offsetX")?.unwrap_or_default();
-    let offset_y = get_attr_int(elem, "offsetY")?.unwrap_or_default();
+    let offset_x: i32 = get_optional_attr(elem, "offsetX")?.unwrap_or_default();
+    let offset_y: i32 = get_optional_attr(elem, "offsetY")?.unwrap_or_default();
     let exc = Err(CelesteMapError { kind: CelesteMapErrorType::OutOfRange, description: format!("{} contains out-of-range data", elem.name)});
     if offset_x < 0 || offset_y < 0 {
         return exc;
@@ -261,7 +251,10 @@ fn parse_fgbg_tiles(elem: &BinEl, width: u32, height: u32) -> Result<Vec<char>, 
     let mut data: Vec<char> = vec!['0'; (width * height) as usize];
     let mut x = offset_x;
     let mut y = offset_y;
-    for ch in get_attr_text(elem, "innerText")?.unwrap_or_default().chars() {
+    for ch in get_optional_attr::<String>(elem, "innerText")?
+        .unwrap_or_default()
+        .chars()
+    {
         if ch == '\n' {
             x = offset_x;
             y += 1;
@@ -279,8 +272,8 @@ fn parse_fgbg_tiles(elem: &BinEl, width: u32, height: u32) -> Result<Vec<char>, 
 }
 
 fn parse_object_tiles(elem: &BinEl, width: u32, height: u32) -> Result<Vec<i32>, CelesteMapError> {
-    let offset_x = get_attr_int(elem, "offsetX")?.unwrap_or_default();
-    let offset_y = get_attr_int(elem, "offsetY")?.unwrap_or_default();
+    let offset_x: i32 = get_optional_attr(elem, "offsetX")?.unwrap_or_default();
+    let offset_y: i32 = get_optional_attr(elem, "offsetY")?.unwrap_or_default();
     let exc = Err(CelesteMapError { kind: CelesteMapErrorType::OutOfRange, description: format!("{} contains out-of-range data", elem.name)});
     if offset_x < 0 || offset_y < 0 {
         return exc;
@@ -291,7 +284,10 @@ fn parse_object_tiles(elem: &BinEl, width: u32, height: u32) -> Result<Vec<i32>,
     let mut data: Vec<i32> = vec![-1; (width * height) as usize];
     let mut y = offset_y;
 
-    for line in get_attr_text(elem, "innerText")?.unwrap_or_default().split('\n') {
+    for line in get_optional_attr::<String>(elem, "innerText")?
+        .unwrap_or_default()
+        .split('\n')
+    {
         let mut x = offset_x;
         for num in line.split(',') {
             if num.is_empty() {
@@ -318,13 +314,15 @@ fn parse_object_tiles(elem: &BinEl, width: u32, height: u32) -> Result<Vec<i32>,
 fn parse_entity_trigger(elem: &BinEl) -> Result<CelesteMapEntity, CelesteMapError> {
     let basic_attrs: Vec<String> = vec!["id".to_string(), "x".to_string(), "y".to_string(), "width".to_string(), "height".to_string()];
     Ok(CelesteMapEntity {
-        id: require_present(get_attr_int(elem, "id"), &elem.name, "id")?,
+        id: get_attr(elem, "id")?,
         name: elem.name.clone(),
-        x: require_present(get_attr_int(elem, "x"), &elem.name, "x")?,
-        y: require_present(get_attr_int(elem, "y"), &elem.name, "y")?,
-        width: get_attr_int(elem, "width")?.unwrap_or(0) as u32,
-        height: get_attr_int(elem, "height")?.unwrap_or(0) as u32,
-        attributes: elem.attributes.iter()
+        x: get_attr(elem, "x")?,
+        y: get_attr(elem, "y")?,
+        width: get_optional_attr(elem, "width")?.unwrap_or(0) as u32,
+        height: get_optional_attr(elem, "height")?.unwrap_or(0) as u32,
+        attributes: elem
+            .attributes
+            .iter()
             .map(|kv| (kv.0.clone(), kv.1.clone()))
             .filter(|kv| !basic_attrs.contains(kv.0.borrow()))
             .collect()
@@ -333,21 +331,21 @@ fn parse_entity_trigger(elem: &BinEl) -> Result<CelesteMapEntity, CelesteMapErro
 
 fn parse_decal(elem: &BinEl) -> Result<CelesteMapDecal, CelesteMapError> {
     Ok(CelesteMapDecal {
-        x: require_present(get_attr_int(elem, "x"), &elem.name, "x")?,
-        y: require_present(get_attr_int(elem, "y"), &elem.name, "y")?,
-        scale_x: require_present(get_attr_float(elem, "scaleX"), &elem.name, "scaleX")?,
-        scale_y: require_present(get_attr_float(elem, "scaleY"), &elem.name, "scaleY")?,
-        texture: require_present(get_attr_text(elem, "texture"), &elem.name, "texture")?,
+        x: get_attr(elem, "x")?,
+        y: get_attr(elem, "y")?,
+        scale_x: get_attr(elem, "scaleX")?,
+        scale_y: get_attr(elem, "scaleY")?,
+        texture: get_attr(elem, "texture")?,
     })
 }
 
 fn parse_filler_rect(elem: & BinEl) -> Result<Rect, CelesteMapError> {
     expect_elem!(elem, "rect");
 
-    let x = require_present(get_attr_int(elem, "x"), &elem.name, "x")?;
-    let y = require_present(get_attr_int(elem, "y"), &elem.name, "y")?;
-    let w = require_present(get_attr_int(elem, "w"), &elem.name, "w")?;
-    let h = require_present(get_attr_int(elem, "h"), &elem.name, "h")?;
+    let x: i32 = get_attr(elem, "x")?;
+    let y: i32 = get_attr(elem, "y")?;
+    let w: i32 = get_attr(elem, "w")?;
+    let h: i32 = get_attr(elem, "h")?;
 
     return Ok(Rect { x: x * 8, y: y * 8, width: w as u32 * 8, height: h as u32 * 8 });
 }
@@ -355,63 +353,104 @@ fn parse_filler_rect(elem: & BinEl) -> Result<Rect, CelesteMapError> {
 fn parse_styleground(elem :&BinEl) -> Result<CelesteMapStyleground, CelesteMapError> {
     Ok(CelesteMapStyleground {
         name: elem.name.clone(),
-        texture: get_attr_text(elem, "texture")?,
-        x: get_attr_int(elem, "x")?,
-        y: get_attr_int(elem, "y")?,
-        loop_x: get_attr_bool(elem, "loopx")?,
-        loop_y: get_attr_bool(elem, "loopy")?,
-        scroll_x: get_attr_float(elem, "scrollx")?,
-        scroll_y: get_attr_float(elem, "scrolly")?,
-        speed_x: get_attr_float(elem, "speedx")?,
-        speed_y: get_attr_float(elem, "speedy")?,
-        color: get_attr_text(elem, "color")?,
-        blend_mode: get_attr_text(elem, "blendmode")?,
+        texture: get_optional_attr(elem, "texture")?,
+        x: get_optional_attr(elem, "x")?,
+        y: get_optional_attr(elem, "y")?,
+        loop_x: get_optional_attr(elem, "loopx")?,
+        loop_y: get_optional_attr(elem, "loopy")?,
+        scroll_x: get_optional_attr(elem, "scrollx")?,
+        scroll_y: get_optional_attr(elem, "scrolly")?,
+        speed_x: get_optional_attr(elem, "speedx")?,
+        speed_y: get_optional_attr(elem, "speedy")?,
+        color: get_optional_attr(elem, "color")?,
+        blend_mode: get_optional_attr(elem, "blendmode")?,
     })
 }
 
-fn get_child<'a>(elem: &'a BinEl, name: &str) -> Result<&'a BinEl, CelesteMapError> {
+fn get_optional_child<'a>(elem: &'a BinEl, name: &str) -> Option<&'a BinEl> {
     let children_of_name = elem.get(name);
-    if children_of_name.len() != 1 {
-        return Err(CelesteMapError::missing_child(elem.name.as_ref(), name));
-    }
-    return Ok(children_of_name.first().unwrap());
+    return if let [ref child] = children_of_name.as_slice() {
+        // if there is exactly one child
+        Some(child)
+    } else {
+        None
+    };
 }
 
-fn get_attr_int(elem: &BinEl, name: &str) -> Result<Option<i32>, CelesteMapError> {
-    return match elem.attributes.get(name) {
-        Some(&BinElAttr::Int(i)) => Ok(Some(i)),
-        Some(&BinElAttr::Float(f)) => Ok(Some(f as i32)),
-        None => Ok(None),
-        Some(attr) => Err(CelesteMapError { kind: CelesteMapErrorType::BadAttrType, description: format!("Expected int, found {:?}", attr) })
-    }
+fn get_child<'a>(elem: &'a BinEl, name: &str) -> Result<&'a BinEl, CelesteMapError> {
+    get_optional_child(elem, name).ok_or_else(|| CelesteMapError::missing_child(&elem.name, name))
 }
 
-fn get_attr_bool(elem: &BinEl, name: &str) -> Result<Option<bool>, CelesteMapError> {
-    return match elem.attributes.get(name) {
-        Some(&BinElAttr::Bool(b)) => Ok(Some(b)),
-        None => Ok(None),
-        Some(attr) => Err(CelesteMapError { kind: CelesteMapErrorType::BadAttrType, description: format!("Expected bool, found {:?}", attr) })
-    }
-}
-
-fn get_attr_float(elem: &BinEl, name: &str) -> Result<Option<f32>, CelesteMapError> {
-    return match elem.attributes.get(name) {
-        Some(&BinElAttr::Float(f)) => Ok(Some(f)),
-        Some(&BinElAttr::Int(i)) => Ok(Some(i as f32)),
-        None => Ok(None),
-        Some(attr) => Err(CelesteMapError { kind: CelesteMapErrorType::BadAttrType, description: format!("Expected float, found {:?}", attr) })
+fn get_optional_attr<T>(elem: &BinEl, name: &str) -> Result<Option<T>, CelesteMapError>
+where
+    T: AttrCoercion,
+{
+    if let Some(attr) = elem.attributes.get(name) {
+        if let Some(coerced) = T::try_coerce(attr) {
+            Ok(Some(coerced))
+        } else {
+            Err(CelesteMapError {
+                kind: CelesteMapErrorType::BadAttrType,
+                description: format!("Expected {nice}, found {:?}", attr, nice = T::NICE_NAME),
+            })
+        }
+    } else {
+        Ok(None)
     }
 }
 
-fn get_attr_text(elem: &BinEl, name: &str) -> Result<Option<String>, CelesteMapError> {
-    return match elem.attributes.get(name) {
-        Some(BinElAttr::Text(s)) => Ok(Some(s.clone())),
-        Some(BinElAttr::Int(i)) => Ok(Some(i.to_string())),
-        None => Ok(None),
-        Some(attr) => Err(CelesteMapError { kind: CelesteMapErrorType::BadAttrType, description: format!("Expected text, found {:?}", attr) })
-    }
+fn get_attr<T>(elem: &BinEl, name: &str) -> Result<T, CelesteMapError>
+where
+    T: AttrCoercion,
+{
+    get_optional_attr(elem, name)?
+        .ok_or_else(|| CelesteMapError::missing_attribute(&elem.name, name))
 }
 
-fn require_present<T>(result: Result<Option<T>, CelesteMapError>, element_name: &str, attribute_name: &str) -> Result<T, CelesteMapError> {
-    result.and_then(|o| o.ok_or_else(||CelesteMapError::missing_attribute(element_name, attribute_name)))
+// Trait for types that a BinElAttr can possibly be coerced to, with the logic to do the coercion
+trait AttrCoercion: Sized {
+    // Type name to print out when giving BadAttrType errors
+    const NICE_NAME: &'static str;
+    fn try_coerce(attr: &BinElAttr) -> Option<Self>;
+}
+
+impl AttrCoercion for i32 {
+    const NICE_NAME: &'static str = "integer";
+    fn try_coerce(attr: &BinElAttr) -> Option<Self> {
+        match *attr {
+            BinElAttr::Int(i) => Some(i),
+            BinElAttr::Float(f) => Some(f as i32),
+            _ => None,
+        }
+    }
+}
+impl AttrCoercion for bool {
+    const NICE_NAME: &'static str = "bool";
+    fn try_coerce(attr: &BinElAttr) -> Option<Self> {
+        if let BinElAttr::Bool(value) = *attr {
+            Some(value)
+        } else {
+            None
+        }
+    }
+}
+impl AttrCoercion for f32 {
+    const NICE_NAME: &'static str = "float";
+    fn try_coerce(attr: &BinElAttr) -> Option<Self> {
+        match *attr {
+            BinElAttr::Float(f) => Some(f),
+            BinElAttr::Int(i) => Some(i as f32),
+            _ => None,
+        }
+    }
+}
+impl AttrCoercion for String {
+    const NICE_NAME: &'static str = "text";
+    fn try_coerce(attr: &BinElAttr) -> Option<Self> {
+        match *attr {
+            BinElAttr::Text(ref s) => Some(s.clone()),
+            BinElAttr::Int(i) => Some(i.to_string()),
+            _ => None,
+        }
+    }
 }
