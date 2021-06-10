d5a261ba48a60dddc042774c3154d2abcd626e0a
Skye Jensen <skyeajensen@gmail.com>
2021-06-10 17:43:36 -0400

    Add AutoSaver wrapper for Config that automatically saves to file on edit
    also some refactors in assets.rs and change signature of autotiler::load

---
+++ src/auto_saver.rs
@@ -0,0 +1,47 @@
+use std::ops::{DerefMut, Deref};
+use std::borrow::{Borrow, BorrowMut};
+
+pub struct AutoSaver<T> {
+    value: T,
+    saver: fn(&mut T),
+}
+impl <T> AutoSaver<T> {
+    pub fn new(value: T, saver: fn(&mut T)) -> Self {
+        Self {
+            value,
+            saver,
+        }
+    }
+    pub fn borrow_mut(&mut self) -> MutRef<T> {
+        MutRef { auto_saver: self }
+    }
+}
+impl<T> Deref for AutoSaver<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+pub struct MutRef<'a, T> {
+    auto_saver: &'a mut AutoSaver<T>,
+}
+impl <T> Drop for MutRef<'_, T> {
+    fn drop(&mut self) {
+        (self.auto_saver.saver)(&mut self.auto_saver.value);
+    }
+}
+impl <T> Deref for MutRef<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.auto_saver.value
+    }
+}
+impl <T> DerefMut for MutRef<'_, T> {
+
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.auto_saver.value
+    }
+}
