54be9aa3c56e5ecaebbef153dc9a79d30820997e
Audrey Dutcher <audrey@rhelmot.io>
2022-01-21 00:51:02 -0700

    Can edit bool attributes. woo.

--- src/tweaker_widget.rs
+++ src/tweaker_widget.rs
@@ -1,14 +1,14 @@
 use std::fmt::{Debug, Formatter};
+use celeste::binel::BinElAttr;
 use vizia::*;
 
 use crate::units::*;
 use crate::app_state::{AppState, AppSelection, AppEvent};
 use crate::map_struct::{CelesteMap, CelesteMapEntity};
+use crate::assets;
 
 #[derive(Copy, Clone, Debug)]
-pub struct CurrentSelectedEntityLens {
-
-}
+pub struct CurrentSelectedEntityLens { }
 
 impl Lens for CurrentSelectedEntityLens {
     type Source = AppState;
@@ -32,21 +32,46 @@ impl CurrentSelectedEntityLens {
     }
 }
 
-pub struct EntityTweakerWidget {
-
-}
+pub struct EntityTweakerWidget { }
 
 impl EntityTweakerWidget {
     pub fn new(cx: &mut Context) -> Handle<'_, Self> {
         Self {
 
         }.build2(cx, move |cx| {
-            Binding::new_fallible(cx, CurrentSelectedEntityLens::new(), |cx, selection| {
-                let entity = selection.get(cx);
-                let msg = format!("Selected entity {}: {}", entity.id, entity.name);
-                Label::new(cx, &msg);
+            let lens = CurrentSelectedEntityLens::new();
+            Binding::new_fallible(cx, lens.clone(), move |cx, selection| {
+                VStack::new(cx, move |cx| {
+                    let entity = selection.get(cx);
+                    let msg = format!("{} - {}", entity.name, entity.id);
+                    let mut attrs = entity.attributes.iter().map(|(a, b)| Some(a.clone())).collect::<Vec<_>>();
+                    Label::new(cx, &msg);
+
+                    ForEach::new(cx, 0..attrs.len(), move |cx, idx| {
+                        let attr = attrs[idx].take().unwrap();
+                        HStack::new(cx, move |cx| {
+                            Label::new(cx, &attr);
+                            let entity = selection.get(cx);
+                            let val = &entity.attributes[&attr];
+                            match val {
+                                BinElAttr::Bool(b) => {
+                                    let b = *b;
+                                    Checkbox::new(cx, b)
+                                        .on_toggle(move |cx| {
+                                            let mut entity = selection.get(cx).clone();
+                                            entity.attributes.insert(attr.clone(), BinElAttr::Bool(!b));
+                                            cx.emit(AppEvent::EntityUpdate { entity });
+                                        });
+                                }
+                                BinElAttr::Int(_) => {}
+                                BinElAttr::Float(_) => {}
+                                BinElAttr::Text(_) => {}
+                            }
+                        });
+                    });
+                });
             }, move |cx| {
-                Label::new(cx, "No entity selected");
+                Element::new(cx);
             });
         })
     }
